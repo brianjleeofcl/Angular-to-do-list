071c20c572279a087695a747b45360d777ee4b7b
Brian Lee <brianjleeofcl@gmail.com>
2017-01-14 23:02:05 -0800

    close #4 Add server routing for sign up

--- routes/users.js
+++ routes/users.js
@@ -5,16 +5,31 @@ const knex = require('../knex');
 const bcrypt = require('bcrypt-as-promised');
 const jwt = require('jsonwebtoken');
 
+const { camelizeKeys, decamelizeKeys } = require('humps');
+
 const express = require('express');
 const router = express.Router();
 
 router.post('/users', (req, res, next) => {
-  // let user;
-  // const { firstName, lastName, email, password, phone } = req.body;
+  let user;
+  const { firstName, lastName, email, password, phone } = req.body;
 
-  console.log(req.body);
-  res.send('got it!')
-})
+  knex('users').where('email', email).then((data) => {
+    if (data.length) {
+      throw new Error('email exists')
+    }
+
+    return bcrypt.hash(password, 12);
+  }).then((hashPw) => {
+    user = { firstName, lastName, email, hashPw, phone };
+
+    return knex('users').insert(decamelizeKeys(user), '*');
+  }).then((array) => {
+    delete user.hashPw;
+
+    res.send(user);
+  }).catch((err) => next(err));
+});
 
 
 module.exports = router;
--- seeds/1_users.js
+++ seeds/1_users.js
@@ -3,15 +3,16 @@ exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('users').del()
     .then(function () {
-      return knex('table_name').insert([{
+      return knex('users').insert([{
         id: 1,
         first_name: 'John',
         last_name: 'Smith',
         email: 'contact@example.com',
-        hash_pw: '',
+        hash_pw: '$2a$12$/uBxx2hmE6arf9EyQfjHC.UWbGWNUi22iMLGrY9Kq4NCO8/KpYWdW',
+        // 'password'
         phone: '+12061112222',
         created_at: new Date('2017-01-14 20:27:00 PST'),
         updated_at: new Date('2017-01-14 20:27:00 PST')
       }])
-    });
+    }).then(() => knex.raw("SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));"));
 };
