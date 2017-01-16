d52cc3afcfe07c1be27c93c54c26c091f313ae8c
Michael Friedman <contact@michaeldavidfriedman.com>
2017-01-16 12:01:39 -0800

    Close #14 Create HTML for Login Page

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,2 +1,32 @@
-$('#login-form').hide();
+(function () {
+  'use strict';
+
+  $.getJSON('/token')
+    .then((loginStatus) => {
+      console.log(loginStatus);
+      if (loginStatus) {
+        window.location.href = '/list.html';
+      } else {
+        $('.login-form').submit((event) => {
+          event.preventDefault();
+
+          const data = {};
+
+          $('input').map((_, dom) => {
+            data[dom.name] = dom.value;
+          });
+
+          const request = {
+            contentType: 'application/json',
+            method: 'POST',
+            dataType: 'json',
+            data: JSON.stringify(data),
+            url: 'users/login',
+          };
+
+          $.ajax(request)
+          .then((res) => { window.location.href = '/list.html'; }, (error) => { console.log(error); });
+        });
+      }
+    }, error => console.log('token error', error));
 })();
