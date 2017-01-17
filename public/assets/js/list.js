2de6bdda74e4e9083a84dc3fa24f431643bb16d2
Michael Friedman <contact@michaeldavidfriedman.com>
2017-01-17 12:42:17 -0800

    Routing for deleting tasks.

--- public/assets/js/list.js
+++ public/assets/js/list.js
@@ -111,7 +111,7 @@
   $('ul').on('click', '.closeIcon', (event) => {
     const taskItem = event.target.parentNode;
     taskItem.remove();
-    const id = $(event.target).attr('id').substr(4);
+    const id = $(event.target.parentNode).attr('id').substr(4);
     const data = JSON.stringify({ id });
     const options = {
       method: 'DELETE',
@@ -119,7 +119,7 @@
       contentType: 'application/json',
       data,
     };
-    
+
     $.ajax(options).then((data) => {
       Materialize.toast('Task removed', 1500);
     }, (error) => {console.log(error)});
