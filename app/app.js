ce6c175fa6fa850b88a8fbf5abee65569655ce26
Brian Lee <brianjleeofcl@gmail.com>
2017-02-02 12:58:44 -0800

    Initialize: gulp, reorganize file structures

--- app/app.js
+++ app/app.js
@@ -7,17 +7,13 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const path = require('path');
-
 const express = require('express');
-
 const app = express();
 
 app.disable('x-powered-by');
 
 const morgan = require('morgan');
-
 const bodyParser = require('body-parser');
-
 const cookieParser = require('cookie-parser');
 
 app.use(morgan('dev'));
@@ -25,18 +21,11 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 const token = require('./routes/token');
-
 const users = require('./routes/users');
-
 const sidenav = require('./routes/sidenav');
-
 const list = require('./routes/list');
-
 const tags = require('./routes/tags');
-
 const shared = require('./routes/shared');
-
-app.use(express.static(path.resolve('public')));
 
 
 app.use(token);
@@ -47,6 +36,9 @@ app.use(list);
 app.use(tags);
 app.use(shared);
 
+app.use('*', function(req, res, next) {
+  res.sendFile('index.html', {root: path.join(__dirname, 'public')})
+});
 // eslint-disable-next-line max-params
 app.use((err, _req, res, _next) => {
   if (err.output && err.output.statusCode) {
@@ -61,11 +53,4 @@ app.use((err, _req, res, _next) => {
   res.sendStatus(500);
 });
 
-const port = process.env.PORT || 8000;
-
-app.listen(port, () => {
-  if (app.get('env') !== 'test') {
-    // eslint-disable-next-line no-console
-    console.log('Listening on port', port);
-  }
-});
+module.exports = app;
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const nodemon = require('gulp-nodemon')
+
+gulp.task('default', ['browser-sync'])
+
+gulp.task('browser-sync', ['nodemon'], function() {
+  browserSync.init({
+    proxy: 'http://localhost:5000',
+    files: ['app/public/**/*.*'],
+    browser: 'google chrome',
+    port: 7000,
+        reloadDelay: 1000,
+  })
+})
+
+gulp.task('nodemon', function (cb) {
+
+  return nodemon({
+    script: './app/bin/www',
+    ignore: [
+      'test/',
+      'public/',
+      'gulpfile.js',
+      'node_modules/'
+    ],
+  }).on('start', function () {
+        setTimeout(cb, 500)
+  })
+})
